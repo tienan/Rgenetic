8d418fe2ce15ef2d631f14e3556ed6cd50927cd3
tienan <tienan_feng@126.com>
2017-06-02 17:30:34 +0800

    add sql

--- database.R
+++ database.R
@@ -28,6 +28,45 @@ Rmysql
 install.packages("RMySQL")
 library(RMySQL)
 
+####For example
+conn <- dbConnect(RMySQL::MySQL(),dbname="health",username="root",password="feng1234",host="172.29.1.30")
 
+
+dbListTables(conn)
+dbWriteTable(conn, "mtcars", mtcars)
+dbWriteTable(conn, "mtcars", mtcars,append=T) 
+dbListTables(conn)
+dbListFields(conn, "mtcars")
+dbReadTable(conn, "mtcars")
+
+# You can fetch all results:
+res <- dbSendQuery(conn, "SELECT * FROM mtcars WHERE cyl = 4")
+dbFetch(res)
+dbClearResult(res)
+
+# Or a chunk at a time
+res <- dbSendQuery(con, "SELECT * FROM mtcars WHERE cyl = 4")
+while(!dbHasCompleted(res)){
+  chunk <- dbFetch(res, n = 5)
+  print(nrow(chunk))
+}
+# Clear the result
+dbClearResult(res)
+
+#提取数据
+dbGetQuery(con, "SELECT * FROM mtcars")
+
+
+# Disconnect from the database
+dbDisconnect(con)
+
+
+#sql 整合临床和基因数据
+
+sql = "select l.sampleID,l.cancer,g.gene_id,g.normalized_count,c.gender,c.race,c.pathologic_T,c.pathologic_N,c.pathologic_M,c.pathologic_stage,c.tobacco_smoking_history,c.days_to_last_followup,c.days_to_death from clinic_luad as c,genes2id_link as l,LUAD_genes as g where c.bcr_patient_barcode=l.patientID and g.sample_id=l.sampleID"
+
+res = dbGetQuery(conn , sql)
+
+write.table(res,file="LUAD_genes_exp.txt",quote = F,sep="\t",row.names = F,fileEncoding = "utf-8")
 
 
